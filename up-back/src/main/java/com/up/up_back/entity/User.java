bd41dbd1919ea5b91741f468cb946779e6be3a5b
Luiza de Souza <souza.luiza@unifesp.br>
2026-06-16 23:38:33 -0300

    User entidade
    com campos de id, nome, email e senha.

---
+++ up-back/src/main/java/com/up/up_back/entity/User.java
@@ -0,0 +1,25 @@
+package com.up.up_back.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
