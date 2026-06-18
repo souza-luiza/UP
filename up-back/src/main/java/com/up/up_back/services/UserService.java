969152f5ec5f61a1c9cba22d2a937f76f7fd235a
Luiza de Souza <souza.luiza@unifesp.br>
2026-06-18 00:05:49 -0300

    Adição de senha criptografada, verificação de email existente e retorno sem senha no UserService.

--- up-back/src/main/java/com/up/up_back/services/UserService.java
+++ up-back/src/main/java/com/up/up_back/services/UserService.java
@@ -1,9 +1,12 @@
 package com.up.up_back.services;
 
-import com.up.up_back.dto.RegisterUserDto;
+import com.up.up_back.dto.user.RegisterUserDto;
+import com.up.up_back.dto.user.UserResponseDto;
 import com.up.up_back.entity.User;
+import com.up.up_back.exception.EmailAlreadyExistsException;
 import com.up.up_back.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,15 +14,26 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public User create(RegisterUserDto dto) {
+    public UserResponseDto create(RegisterUserDto dto) {
+
+        if (userRepository.findByEmail(dto.email()).isPresent()) {
+            throw new EmailAlreadyExistsException("Email already registered");
+        }
 
         User user = User.builder()
                 .name(dto.name())
                 .email(dto.email())
-                .password(dto.password())
+                .password(passwordEncoder.encode(dto.password()))
                 .build();
 
-        return userRepository.save(user);
+        User savedUser = userRepository.save(user);
+
+        return new UserResponseDto(
+                savedUser.getId(),
+                savedUser.getName(),
+                savedUser.getEmail()
+        );
     }
 }
