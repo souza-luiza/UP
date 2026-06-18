4d53fc64b7b4aaa4d8f2823e14b6b0d5883f5491
Luiza de Souza <souza.luiza@unifesp.br>
2026-06-18 00:07:43 -0300

    Testes de integração para cadastro de usuários + configurações para testes de integração.
    Testando cadastro com sucesso, email inválido, senha muito curta, nome em branco e email duplicado.
    Testes de integração feito com banco de dados em memória H2.

---
+++ up-back/src/test/java/com/up/up_back/integration/UserControllerIntegrationTest.java
@@ -0,0 +1,114 @@
+package com.up.up_back.integration;
+
+import com.up.up_back.dto.user.RegisterUserDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class UserControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void shouldCreateUserSuccessfully() throws Exception {
+
+        RegisterUserDto dto = new RegisterUserDto(
+                "Joao",
+                "joao@email.com",
+                "123456"
+        );
+
+        mockMvc.perform(
+                post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto))
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturnBadRequestWhenEmailIsInvalid() throws Exception {
+
+        RegisterUserDto dto = new RegisterUserDto(
+                "Maria",
+                "email-invalido",
+                "123456"
+        );
+
+        mockMvc.perform(
+                post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto))
+        ).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void shouldReturnBadRequestWhenPasswordIsTooShort() throws Exception {
+
+        RegisterUserDto dto = new RegisterUserDto(
+                "Maria",
+                "maria@email.com",
+                "12345"
+        );
+
+        mockMvc.perform(
+                post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto))
+        ).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void shouldReturnBadRequestWhenNameIsBlank() throws Exception {
+
+        RegisterUserDto dto = new RegisterUserDto(
+                "",
+                "maria@email.com",
+                "123456"
+        );
+
+        mockMvc.perform(
+                post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto))
+        ).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void shouldReturnConflictWhenEmailAlreadyExists() throws Exception {
+
+        RegisterUserDto dto = new RegisterUserDto(
+                "Maria",
+                "maria@email.com",
+                "123456"
+        );
+
+        mockMvc.perform(
+                post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto))
+        );
+
+        mockMvc.perform(
+                post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto))
+        ).andExpect(status().isConflict());
+    }
+
+}
