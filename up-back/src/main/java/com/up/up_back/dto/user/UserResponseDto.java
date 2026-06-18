20d9319fd273731b851bce2ec7350efe14e9694a
Luiza de Souza <souza.luiza@unifesp.br>
2026-06-18 00:04:23 -0300

    Add passwordEncoder, UserResponseDto e validações em User entity.
    passwordEncoder para criptografia de senha; dto para resposta da requisição não retornar a senha; validações na entidade User.

---
+++ up-back/src/main/java/com/up/up_back/dto/user/UserResponseDto.java
@@ -0,0 +1,8 @@
+package com.up.up_back.dto.user;
+
+public record UserResponseDto(
+        Long id,
+        String name,
+        String email
+) {
+}
--- up-back/src/main/java/com/up/up_back/entity/User.java
+++ up-back/src/main/java/com/up/up_back/entity/User.java
@@ -9,17 +9,19 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 }
