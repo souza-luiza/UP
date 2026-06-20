0533533bfd356ac65a2a43318c37a9abffd2c7b4
Luiza de Souza <souza.luiza@unifesp.br>
2026-06-20 00:55:03 -0300

    JwtService para criação JWT com access token e refresh token

---
+++ up-back/src/main/java/com/up/up_back/security/JwtService.java
@@ -0,0 +1,64 @@
+package com.up.up_back.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${api.security.token.secret}")
+    private String SECRET;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateAccessToken(String email) {
+        SecretKey key = getSigningKey();
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000L * 60 * 15))
+                .signWith(key)
+                .compact();
+    }
+
+    public String generateRefreshToken(String email) {
+        SecretKey key = getSigningKey();
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 7))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        SecretKey key = getSigningKey();
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean isValid(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(getSigningKey())
+                    .build()
+                    .parseSignedClaims(token);
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
