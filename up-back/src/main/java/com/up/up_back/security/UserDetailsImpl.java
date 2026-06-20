6689cc594a7254bcebd45645b206a855a3634e5f
Luiza de Souza <souza.luiza@unifesp.br>
2026-06-20 00:49:19 -0300

    Add UserDetailsImpl para o Spring Security

---
+++ up-back/src/main/java/com/up/up_back/security/UserDetailsImpl.java
@@ -0,0 +1,32 @@
+package com.up.up_back.security;
+
+import com.up.up_back.entity.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
