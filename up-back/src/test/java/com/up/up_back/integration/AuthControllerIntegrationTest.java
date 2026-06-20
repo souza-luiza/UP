3a9587c23e1aab13146036d567fdcc0b9455e7ea
Luiza de Souza <souza.luiza@unifesp.br>
2026-06-20 01:32:34 -0300

    Teste de integração de autentificação
    - Login com sucesso;
    - unauthorized com senha invalida;
    - unauthorized quando user nao existe.

---
+++ up-back/src/test/java/com/up/up_back/integration/AuthControllerIntegrationTest.java
@@ -0,0 +1,109 @@
+package com.up.up_back.integration;
+
+import com.up.up_back.dto.auth.LoginRequestDto;
+import com.up.up_back.entity.User;
+import com.up.up_back.repository.RefreshTokenRepository;
+import com.up.up_back.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(properties = {"api.security.token.secret=chave_secreta_para_os_testes_de_integracao_do_projeto_up_com_mais_de_32_bytes"})
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class AuthControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RefreshTokenRepository refreshTokenRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setup() {
+
+        refreshTokenRepository.deleteAll();
+        userRepository.deleteAll();
+
+        User user = User.builder()
+                .name("Joao")
+                .email("joao@gmail.com")
+                .password(passwordEncoder.encode("123456"))
+                .build();
+
+        userRepository.save(user);
+    }
+
+    @Test
+    void shouldLoginSuccessfully() throws Exception {
+
+        LoginRequestDto dto = new LoginRequestDto(
+                "joao@gmail.com",
+                "123456"
+        );
+
+        mockMvc.perform(
+                post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto))
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").exists())
+                .andExpect(header().exists(HttpHeaders.SET_COOKIE))
+                .andExpect(header().string(HttpHeaders.SET_COOKIE, containsString("refreshToken=")));
+    }
+
+    @Test
+    void shouldReturnUnauthorizedWhenPasswordIsInvalid() throws Exception {
+
+        LoginRequestDto dto = new LoginRequestDto(
+                        "joao@gmail.com",
+                        "senha-errada"
+                );
+
+        mockMvc.perform(
+                        post("/auth/login")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(dto))
+                )
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void shouldReturnUnauthorizedWhenUserDoesNotExist() throws Exception {
+
+        LoginRequestDto dto = new LoginRequestDto(
+                "inexistente@gmail.com",
+                "123456"
+        );
+
+        mockMvc.perform(
+                        post("/auth/login")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(dto))
+                )
+                .andExpect(status().isUnauthorized());
+    }
+}
