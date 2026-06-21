9f6b20b5d9b4b9844105ae71c9a4e59274f48def
Luiza de Souza <souza.luiza@unifesp.br>
2026-06-21 18:05:08 -0300

    Endpoint /users/me
    Retorna dados do usuário atual.

--- up-back/src/main/java/com/up/up_back/controller/UserController.java
+++ up-back/src/main/java/com/up/up_back/controller/UserController.java
@@ -2,9 +2,12 @@ package com.up.up_back.controller;
 
 import com.up.up_back.dto.user.RegisterUserDto;
 import com.up.up_back.dto.user.UserResponseDto;
+import com.up.up_back.entity.User;
+import com.up.up_back.security.UserDetailsImpl;
 import com.up.up_back.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,5 +20,14 @@ public class UserController {
     @PostMapping
     public UserResponseDto create(@Valid @RequestBody RegisterUserDto dto) {
         return userService.create(dto);
+    }
+
+    @GetMapping("/me")
+    public UserResponseDto me(Authentication authentication) {
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        User user = userDetails.getUser();
+
+        return new UserResponseDto(user.getId(), user.getName(), user.getEmail());
     }
 }
