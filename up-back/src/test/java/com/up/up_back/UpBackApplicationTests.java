d434cc46e3370de6256afa6f420eb16aa444635c
Luiza de Souza <souza.luiza@unifesp.br>
2026-06-21 18:05:30 -0300

    Configurações de testes

--- up-back/src/test/java/com/up/up_back/UpBackApplicationTests.java
+++ up-back/src/test/java/com/up/up_back/UpBackApplicationTests.java
@@ -2,8 +2,10 @@ package com.up.up_back;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {"api.security.token.secret=chave_secreta_para_os_testes_de_integracao_do_projeto_up_com_mais_de_32_bytes"})
+@ActiveProfiles("test")
 class UpBackApplicationTests {
 
 	@Test
