77ecb545e2e52711a3d0287fab61b55ad54ebc2e
Luiza de Souza <souza.luiza@unifesp.br>
2026-06-21 18:04:37 -0300

    Add filtro JWT auth e update security config
    rotas que não precisam de token: /users, /auth/login e /auth/refresh.

--- up-back/src/main/java/com/up/up_back/config/SecurityConfig.java
+++ up-back/src/main/java/com/up/up_back/config/SecurityConfig.java
@@ -1,25 +1,35 @@
 package com.up.up_back.config;
 
+import com.up.up_back.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(
-            HttpSecurity http
-    ) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers("/users", "/auth/login", "/auth/refresh")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
                 )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
