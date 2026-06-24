c2085439b55f118efc3f2c76cd6cdaa348722653
Luiza de Souza <souza.luiza@unifesp.br>
2026-06-24 18:12:42 -0300

    mudança em button para todos ficarem do mesmo tamanho

--- up-front/src/app/components/Button.tsx
+++ up-front/src/app/components/Button.tsx
@@ -8,12 +8,12 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
 export default function Button({ children, className, variant = 'primary', href, ...props }: ButtonProps) {
 
     const variantStyles = {
-        primary: 'bg-primary text-black hover:shadow-[0_0_8px_#B39EB5]',
-        secondary: 'bg-secondary text-black hover:shadow-[0_0_8px_#ADEBB3]',
-        outline: 'border-2 border-black text-black  hover:shadow-[0_0_4px_#000000]'
+        primary: 'bg-primary text-black border-2 border-transparent hover:shadow-[0_0_8px_#B39EB5]',
+        secondary: 'bg-secondary text-black border-2 border-transparent hover:shadow-[0_0_8px_#ADEBB3]',
+        outline: 'border-2 border-black text-black hover:shadow-[0_0_4px_#000000]'
     }[variant];
 
-    const classes = `flex justify-center items-center py-2 px-5 rounded-md-custom text-p font-regular transition-all duration-150 ease-in-out ${variantStyles} ${className || ''}`;
+    const classes = `flex justify-center items-center py-1.5 px-5 rounded-md-custom text-p font-regular transition-all duration-150 ease-in-out ${variantStyles} ${className || ''}`;
 
     if(href) {
         return (
