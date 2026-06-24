baee6db799326ce76a5205839353a742a55cdfe3
Luiza de Souza <souza.luiza@unifesp.br>
2026-06-24 14:54:40 -0300

    Componente de Botão

---
+++ up-front/src/app/components/Button.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    variant?: 'primary' | 'secondary' | 'outline';
+    href?: string;
+}
+
+export default function Button({ children, className, variant = 'primary', href, ...props }: ButtonProps) {
+
+    const variantStyles = {
+        primary: 'bg-primary text-black hover:shadow-[0_0_8px_#B39EB5]',
+        secondary: 'bg-secondary text-black hover:shadow-[0_0_8px_#ADEBB3]',
+        outline: 'border-2 border-black text-black  hover:shadow-[0_0_4px_#000000]'
+    }[variant];
+
+    const classes = `flex justify-center items-center py-2 px-5 rounded-md-custom text-b1 font-regular transition-all duration-150 ease-in-out ${variantStyles} ${className || ''}`;
+
+    if(href) {
+        return (
+            <Link href={href} className={classes}>
+                {children}
+            </Link>
+        )
+    }
+
+    return(
+        <button {...props} className={classes}>
+            {children}
+        </button>
+    )
+}
