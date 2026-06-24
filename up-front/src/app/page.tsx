f01742b6350a7992bdd7f5725a208e7ccae434e0
Luiza de Souza <souza.luiza@unifesp.br>
2026-06-24 14:54:53 -0300

    Header finalizada

--- up-front/src/app/page.tsx
+++ up-front/src/app/page.tsx
@@ -1,11 +1,19 @@
-import Image from "next/image";
+import Button from "./components/Button";
 import Logo from "./components/Logo";
 
 export default function Home() {
   return (
     <div className="w-full flex min-h-screen flex-col items-center justify-between">
-      <header className="flex bg-primary py-[5px] px-[50px] justify-between items-center self-stretch">
+      <header className="flex w-full bg-primary py-1.5 px-1 md:px-20 justify-between items-center">
         <Logo primaryColor={false}/>
+        <nav className="flex gap-4 items-center">
+          <Button variant="secondary">
+            Cadastrar
+          </Button>
+          <Button variant="outline">
+            Acessar conta
+          </Button>
+        </nav>
       </header>
       <main></main>
       <footer></footer>
