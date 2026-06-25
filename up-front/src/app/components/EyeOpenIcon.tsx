26af74a5b2c28d6f055c637e7fae6927795954f2
akemiikemoto <leticia.ikemoto04@gmail.com>
2026-06-25 01:47:38 -0300

    modificações do icone EyeClosed e Globals
    adição de F5EEF5 e 5C4F5E

--- up-front/src/app/components/EyeOpenIcon.tsx
+++ up-front/src/app/components/EyeOpenIcon.tsx
@@ -6,8 +6,8 @@ export default function EyeOpen({ primaryColor }: EyeOpenProps) {
     return (
         <div className="inline-flex justify-center items-center">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="14" viewBox="0 0 24 18" fill="none">
-                <path d="M1 9C1 9 5 1 12 1C19 1 23 9 23 9C23 9 19 17 12 17C5 17 1 9 1 9Z" stroke="#3B2E3B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                <path d="M12 12C13.6569 12 15 10.6569 15 9C15 7.34315 13.6569 6 12 6C10.3431 6 9 7.34315 9 9C9 10.6569 10.3431 12 12 12Z" stroke="#3B2E3B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M1 9C1 9 5 1 12 1C19 1 23 9 23 9C23 9 19 17 12 17C5 17 1 9 1 9Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M12 12C13.6569 12 15 10.6569 15 9C15 7.34315 13.6569 6 12 6C10.3431 6 9 7.34315 9 9C9 10.6569 10.3431 12 12 12Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
         </div>
     );
