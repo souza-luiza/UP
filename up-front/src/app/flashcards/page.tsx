dfe9bf03da04803ccc9b3eeae385d3ad35ada6b2
Luiza de Souza <souza.luiza@unifesp.br>
2026-06-25 05:30:27 -0300

    Página de Flashcards

---
+++ up-front/src/app/flashcards/page.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { useState } from "react";
+import Button from "../../components/Button";
+import Logo from "../../components/Logo";
+import TextInput from "../../components/TextInput";
+import CardFlashcard from "@/components/CardFlashcard";
+
+export default function Flashcards() {
+    const [question, setQuestion] = useState('');
+    const [answer, setAnswer] = useState('');
+
+    const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        // CHAMA PARA API
+
+        // Limpa os campos após o envio
+        setQuestion('');
+        setAnswer('');
+    };
+
+    return(
+        <div className="flex min-h-screen flex-col">
+            <header className="flex w-full bg-primary py-1.5 px-1 md:px-20 justify-between items-center">
+            
+                <Logo primaryColor={false}/>
+        
+                <nav className="flex gap-4 items-center">
+                    <Button variant="secondary">
+                    Sair da conta
+                    </Button>
+                </nav>
+            
+            </header>
+
+            <main className="flex flex-col p-6 gap-3 md:px-20">
+                <h2 className="text-h2 text-black font-semibold">Flashcards</h2>
+
+                <Button variant="outline">
+                    Revisar Flashcards
+                </Button>
+
+                <div className="bg-primary rounded-md-custom p-3 flex flex-col gap-3">
+                    <h4 className="text-h4 text-black font-semibold">Cadastre flashcards para revisar conteúdos utilizando repetição espaçada.</h4>
+                    
+                    <form onSubmit={handleSubmit} className="flex flex-col items-start gap-4">
+                        <TextInput 
+                            id="question"
+                            label="Pergunta"
+                            type="text"
+                            placeholder="Digite a pergunta do flashcard"
+                            value={question}
+                            onChange={(e) => setQuestion(e.target.value)}
+                            required
+                        />
+
+                        <TextInput 
+                            id="answer"
+                            label="Resposta"
+                            type="text"
+                            placeholder="Digite a resposta do flashcard"
+                            value={answer}
+                            onChange={(e) => setAnswer(e.target.value)}
+                            required
+                        />
+
+                        <Button type="submit" variant="secondary">
+                            Adicionar Flashcard
+                        </Button>
+                    </form>
+                </div>
+
+                <h4 className="text-h4 text-black font-semibold">Flashcards cadastrados</h4>
+                <div className="flex flex-wrap items-stretch gap-6 flex-col md:flex-row self-stretch">
+                    <CardFlashcard 
+                        question="O que é encapsulamento?" 
+                        answer="Ocultar detalhes internos de uma classe e expor apenas o necessário através de métodos públicos."
+                        level={0}
+                        onDelete={() => console.log("Deletar este card")}
+                    />
+                    <CardFlashcard 
+                        question="Qual a diferença entre Let e Const?" 
+                        answer="Let permite reatribuição de valor, enquanto Const cria uma referência imutável."
+                        level={1}
+                        onDelete={() => console.log("Deletar este card")}
+                    />
+                </div>
+
+
+
+            </main>
+
+        </div>
+    )
+}
